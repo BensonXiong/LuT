9d5e342a2309745e7af62be35188823623c1458a
BensonXiong <benson_xiong@sina.com>
2016-02-26 16:25:03 +0800

    a

--- app/src/main/java/com/lu/qa/lut/MainActivity.java
+++ app/src/main/java/com/lu/qa/lut/MainActivity.java
@@ -5,6 +5,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends Activity {
@@ -12,29 +22,34 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ListView listview = new ListView(this);
+        SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.activity_main,new String[]{"title","info","img"},new int[]{R.id.title,R.id.info,R.id.img});
+
+        listview.setAdapter(adapter);
+        setContentView(listview);
+
     }
 
+    private List<Map<String,Object>> getData(){
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "G1");
+        map.put("info", "google 1");
+        map.put("img", R.drawable.i1);
+        list.add(map);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        map = new HashMap<String, Object>();
+        map.put("title", "G2");
+        map.put("info", "google 2");
+        map.put("img", R.drawable.i2);
+        list.add(map);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.i3);
+        list.add(map);
+        return list;
     }
 }

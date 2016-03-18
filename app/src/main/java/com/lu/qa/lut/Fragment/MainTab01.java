319e0b2e12443bc2232c3f4fb0179d3e00b8016e
BensonXiong <benson_xiong@sina.com>
2016-03-18 12:45:06 +0800

    a

--- app/src/main/java/com/lu/qa/lut/Fragment/MainTab01.java
+++ app/src/main/java/com/lu/qa/lut/Fragment/MainTab01.java
@@ -6,8 +6,15 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.lu.qa.lut.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,8 +31,35 @@ public class MainTab01 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_tab01, container, false);
+        View view= inflater.inflate(R.layout.fragment_main_tab01,container,false);
+        ListView lv = (ListView) view.findViewById(R.id.main_tab01_list);
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(),getData(),R.layout.activity_main,new String[]{"title","info","img"},new int[]{R.id.title,R.id.info,R.id.img});
+
+        lv.setAdapter(adapter);
+        return view;
     }
 
+    private List<Map<String,Object>> getData(){
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "G1");
+        map.put("info", "google 1");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "G2");
+        map.put("info", "google 2");
+        map.put("img", R.drawable.i2);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.i3);
+        list.add(map);
+        return list;
+    }
 
 }
